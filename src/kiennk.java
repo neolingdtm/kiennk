161b7e506e206b88ee3626cf40b8c8e0375f76f0
kiennk <kiennk@ttc-solutions.com>
2021-12-10 17:13:32 +0700

    aa

--- src/kiennk.java
+++ src/kiennk.java
@@ -99,6 +99,7 @@ public class kiennk {
     public static long getDateDiff(Date date1, Date date2, TimeUnit timeUnit) {
         long diffInMillies = date2.getTime() - date1.getTime();
         return timeUnit.convert(diffInMillies, TimeUnit.MILLISECONDS);
+        //
     }
 
     @Override
